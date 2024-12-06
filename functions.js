527df2365c9c4ae5a4c56b932e300dfe5f695ca9
Jonny McIlroy <jonnymcilroy063@gmail.com>
2024-12-06 15:25:35 +0000

    operate function. divide by zero error

--- functions.js
+++ functions.js
@@ -11,6 +11,29 @@ export function multiply(num1, num2) {
 }
 
 export function divide(num1, num2) {
+    if (num2 === 0) {
+        return 'error'
+    }
     return num1 / num2;
 }
 
+export function operate(num1, operator, num2) {
+    let answer;
+    switch (operator) {
+        case '+':
+            answer = add(num1, num2);
+            break;
+        case '-':
+            answer = subtract(num1, num2);
+            break;
+        case '*':
+            answer = multiply(num1, num2);
+            break;
+        case '/':
+            answer = divide(num1, num2);
+            break;
+        default:
+            answer = 'error'
+    }
+    return answer;
+}
