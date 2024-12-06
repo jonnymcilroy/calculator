c1312e2a71c75786c6d713d52b23fb72e9c9f737
Jonny McIlroy <jonnymcilroy063@gmail.com>
2024-12-06 17:34:32 +0000

    calculator html and css

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { add, subtract, multiply, divide } from './functions.js'
+import { add, subtract, multiply, divide, operate } from './functions.js'
 
 console.log(add(1, 1))
 
@@ -6,4 +6,6 @@ console.log(subtract(1, 1))
 
 console.log(multiply(1, 1))
 
-console.log(divide(2, 2))
+console.log(divide(2, 0))
+
+console.log(operate(1, '/', 2))
